225df1c7333efdacee5330280b34ec25e932b241
sammy300339 <samwelmilson23@gmail.com>
2022-02-17 02:32:31 +0300

    Initial Commit

--- school_management/settings.py
+++ school_management/settings.py
@@ -80,13 +80,13 @@ WSGI_APPLICATION = 'school_management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE':'django.db.backends.mysql',
-        # 'NAME':'student_management_system',
-        # 'USER':'student_management_system',
-        # 'PASSWORD':'student_management_password',
+        'NAME': os.path.join(BASE_DIR ,'db.sqlite3'),
+        # 'ENGINE':'django.db.backends.postgresql',
+        # 'NAME':'student_management',
+        # 'USER':'student_management',
+        # 'PASSWORD':'blackboltr45',
         # 'HOST':'localhost',
-        # 'PORT':'3306'
+        # 'PORT':'5432'
     }
 }
 

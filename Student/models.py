98d74afa38a3b90ae8d63fbbc5588ae399f937e6
sammy300339 <samwelmilson23@gmail.com>
2022-05-02 17:28:47 +0300

    Initial Commit

--- Student/models.py
+++ Student/models.py
@@ -29,7 +29,7 @@ class hod(models.Model):
     address = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    fcm_token=models.TextField(default="")
+    # fcm_token=models.TextField(default="")
     objects=models.Manager()
 
 
